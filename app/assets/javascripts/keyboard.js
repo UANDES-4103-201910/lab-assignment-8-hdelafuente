241b362f57a28350305cb527e2ccfbebd7606db0
hdelafuente <hadelafuente@miuandes.c>
2019-05-08 10:00:52 -0400

    master: * Keyboard writes, Bkps deletes, Enter adds new line * Button Hide/Show does what it is meant to do

--- app/assets/javascripts/keyboard.js
+++ app/assets/javascripts/keyboard.js
@@ -1,4 +1,31 @@
 // Note: $(() => {}); is equivalent to $(document).ready(function(){})
-$(() => {
-	console.log('Document ready! [app/assets/javascripts/keyboard.js]');
+$(document).ready(function () {
+    $('.key').click(function () {
+        var keyValue = $(this).attr('value');
+        if (keyValue == "Bksp"){
+            var wText = $('#written_text').text();
+            var newText = wText.slice(0, wText.length - 1);
+            $('#written_text').empty();
+            $('#written_text').append(newText);
+        }
+        else if (keyValue == "Enter"){
+            $('#written_text').append("<br><br>");
+        }
+        else {
+            $('#written_text').append(keyValue);
+        }
+        console.log(keyValue);
+    });
+    var hidden = 0;
+    $('#btn-show-hide').click(function () {
+        if (hidden == 0) {
+            $('#kcontainer').hide();
+            hidden += 1;
+        }
+        else {
+            $('#kcontainer').show();
+            hidden -= 1;
+        }
+    })
 });
+
